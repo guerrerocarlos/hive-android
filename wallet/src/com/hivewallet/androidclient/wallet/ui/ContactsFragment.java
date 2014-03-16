33844426fd20ea3f4b59a8c2232833449d82206d
Jan Vornberger <jan@uos.de>
2014-03-16 23:48:17 +0100

    Hook up send button in contact list

--- wallet/src/com/hivewallet/androidclient/wallet/ui/ContactsFragment.java
+++ wallet/src/com/hivewallet/androidclient/wallet/ui/ContactsFragment.java
@@ -1,10 +1,12 @@
 package com.hivewallet.androidclient.wallet.ui;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.google.bitcoin.core.AddressFormatException;
 import com.hivewallet.androidclient.wallet.AddressBookProvider;
 import com.hivewallet.androidclient.wallet.util.PhoneContactsLookupToolkit;
 import com.hivewallet.androidclient.wallet_test.R;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -23,6 +25,8 @@ import android.widget.ListView;
 
 public class ContactsFragment extends SherlockFragment implements LoaderCallbacks<Cursor>
 {
+	private WalletActivity activity;
+	
 	private ListView contactsListView;
 	private ImageButton addContactImageButton;
 	
@@ -43,8 +47,11 @@ public class ContactsFragment extends SherlockFragment implements LoaderCallback
 	{
 		super.onActivityCreated(savedInstanceState);
 
-		final String[] from_columns = { AddressBookProvider.KEY_LABEL, AddressBookProvider.KEY_LABEL };
-		final int[] to_ids = { R.id.tv_contact_name, R.id.iv_contact_photo };
+		final String[] from_columns = { AddressBookProvider.KEY_LABEL
+									  , AddressBookProvider.KEY_LABEL
+									  , AddressBookProvider.KEY_ADDRESS
+									  };
+		final int[] to_ids = { R.id.tv_contact_name, R.id.iv_contact_photo, R.id.ib_contact_send_money };
 		contactsSimpleCursorAdapter = new SimpleCursorAdapter
 				( getActivity()
 				, R.layout.contacts_list_item
@@ -62,18 +69,19 @@ public class ContactsFragment extends SherlockFragment implements LoaderCallback
 				switch (view.getId()) {
 					case R.id.iv_contact_photo:
 						return setContactPhoto((ImageView)view, cursor, columnIndex);
+					case R.id.ib_contact_send_money:
+						return setSendMoneyButton((ImageButton)view, cursor, columnIndex);
 					default:
 						return false;
 				}
 			}
 			
-			private boolean setContactPhoto(ImageView view, Cursor cursor, int columnIndex) {
+			private boolean setContactPhoto(ImageView imageView, Cursor cursor, int columnIndex) {
 				String label = cursor.getString(columnIndex);
 				
 				// this query runs on the UI thread - should probably be changed
 				// to run in the background at some point
 				Uri uri = PhoneContactsLookupToolkit.lookupPhoneContactPicture(getActivity(), label);
-				ImageView imageView = (ImageView)view;
 				if (uri != null) {
 					imageView.setImageURI(uri);
 				} else {
@@ -82,6 +90,26 @@ public class ContactsFragment extends SherlockFragment implements LoaderCallback
 	    	
 		    	return true;
 			}
+			
+			private boolean setSendMoneyButton(ImageButton imageButton, Cursor cursor, int columnIndex) {
+				final String address = cursor.getString(columnIndex);
+				final String label = cursor.getString(cursor.getColumnIndexOrThrow(AddressBookProvider.KEY_LABEL));
+				imageButton.setOnClickListener(new OnClickListener()
+				{
+					@Override
+					public void onClick(View v)
+					{
+						try { 
+							activity.handleSendCoinsToAddress(address, label);
+						} catch (AddressFormatException e) {
+							// if something is wrong with the address, ignore it
+							// and go to the send coin screen anyway
+							activity.handleSendCoins();
+						}
+					}
+				});
+				return true;
+			}
 		});		
 						
 		contactsListView = (ListView) getActivity().findViewById(R.id.lv_contacts);
@@ -89,6 +117,14 @@ public class ContactsFragment extends SherlockFragment implements LoaderCallback
 		
 		getActivity().getSupportLoaderManager().initLoader(0, null, this);
 	}
+	
+	@Override
+	public void onAttach(final Activity activity)
+	{
+		super.onAttach(activity);
+
+		this.activity = (WalletActivity) activity;
+	}	
 
 	private OnClickListener addContactOnClickListener = new OnClickListener()
 	{
