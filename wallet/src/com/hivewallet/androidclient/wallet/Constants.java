6ae0e24b4fa175fbe288aa4e18eb8c27e66d2500
Jan Vornberger <jan@uos.de>
2014-05-08 17:02:13 +0200

    Change default name of backup file

--- wallet/src/com/hivewallet/androidclient/wallet/Constants.java
+++ wallet/src/com/hivewallet/androidclient/wallet/Constants.java
@@ -46,7 +46,7 @@ public class Constants
 	public static final String WALLET_KEY_BACKUP_PROTOBUF = "key-backup-protobuf" + FILENAME_NETWORK_SUFFIX;
 
 	public static final File EXTERNAL_WALLET_BACKUP_DIR = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-	public static final String EXTERNAL_WALLET_KEY_BACKUP = "bitcoin-wallet-keys" + FILENAME_NETWORK_SUFFIX;
+	public static final String EXTERNAL_WALLET_KEY_BACKUP = "hive-android-wallet-keys" + FILENAME_NETWORK_SUFFIX;
 
 	public static final String BLOCKCHAIN_FILENAME = "blockchain" + FILENAME_NETWORK_SUFFIX;
 
